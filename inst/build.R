ef6fd1fd4f3e65ac801e615483e7164b72bac014
ruilin <ruilinli@stanford.edu>
2020-10-14 17:02:08 -0700

    plink2 version of wls seems to be working, needed matrix interface in R

--- inst/build.R
+++ inst/build.R
@@ -1,3 +1,6 @@
 tools::package_native_routine_registration_skeleton('/Users/ruilinli/glmnet', con='/Users/ruilinli/glmnet/src/glmnet_init.c')
 devtools::document('/Users/ruilinli/glmnet')
 install.packages('/Users/ruilinli/glmnet', repo=NULL, type='source')
+
+library(glmnet)
+a = list('/Users/ruilinli/plink-ng/toy_data.pgen')
